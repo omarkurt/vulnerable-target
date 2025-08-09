8ce93db585234fd9ba88d801986dd6a1575acad5
Omar Kurt <om4rkurt@gmail.com>
2025-08-09 15:36:02 +0300

    feat(utils): add banner and quote

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/happyhackingspace/vulnerable-target/internal/logger"
 	"github.com/happyhackingspace/vulnerable-target/internal/options"
+	"github.com/happyhackingspace/vulnerable-target/internal/utils"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -34,14 +35,24 @@ func init() {
 var rootCmd = &cobra.Command{
 	Use:     "vt",
 	Short:   "Create vulnerable environment",
-	Version: "1.0.0",
-	PersistentPreRun: func(_ *cobra.Command, _ []string) {
+	Version: utils.AppVersion,
+	PersistentPreRun: func(cmd *cobra.Command, _ []string) {
 		logger.Init()
+		if cmd.Name() != "help" {
+			fmt.Println(utils.Banner())
+		}
 	},
 	SilenceErrors: true,
 }
 
 func Run() {
+
+	originalHelp := rootCmd.HelpFunc()
+	rootCmd.SetHelpFunc(func(c *cobra.Command, s []string) {
+		fmt.Println(utils.Banner())
+		originalHelp(c, s)
+	})
+
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal().Msg(err.Error())
 	}
