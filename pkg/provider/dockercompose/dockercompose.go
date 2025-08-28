899378970efaf8f2e6c7845cf94d73de4ebd93ef
Recep Gunes <recepgunes2@outlook.com>
2025-08-29 00:08:58 +0300

    use compose lib instead of os exec

--- pkg/provider/dockercompose/dockercompose.go
+++ pkg/provider/dockercompose/dockercompose.go
@@ -1,11 +1,10 @@
 package dockercompose
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
-	"path/filepath"
+	"context"
 
+	"github.com/docker/compose/v2/pkg/api"
+	"github.com/docker/compose/v2/pkg/compose"
 	"github.com/happyhackingspace/vulnerable-target/pkg/provider"
 	"github.com/happyhackingspace/vulnerable-target/pkg/templates"
 )
@@ -19,17 +18,17 @@ func (d *DockerCompose) Name() string {
 }
 
 func (d *DockerCompose) Start(template *templates.Template) error {
-	path := template.Providers["docker-compose"].Path
-	composePath, err := d.resolveComposePath(template.ID, path)
+	dockerCli, err := createDockerCLI()
 	if err != nil {
 		return err
 	}
 
-	upCmd := exec.Command("docker", "compose", "-f", composePath, "-p", fmt.Sprintf("vt-compose-%s", template.ID), "up", "-d") // #nosec G204
-	upCmd.Stdout = os.Stdout
-	upCmd.Stderr = os.Stderr
+	project, err := loadComposeProject(*template)
+	if err != nil {
+		return err
+	}
 
-	err = upCmd.Run()
+	err = runComposeUp(dockerCli, project)
 	if err != nil {
 		return err
 	}
@@ -38,39 +37,26 @@ func (d *DockerCompose) Start(template *templates.Template) error {
 }
 
 func (d *DockerCompose) Stop(template *templates.Template) error {
-	path := template.Providers["docker-compose"].Path
-	composePath, err := d.resolveComposePath(template.ID, path)
+	dockerCli, err := createDockerCLI()
 	if err != nil {
 		return err
 	}
 
-	upCmd := exec.Command("docker", "compose", "-f", composePath, "-p", fmt.Sprintf("vt-compose-%s", template.ID), "down", "--volumes") // #nosec G204
-	upCmd.Stdout = os.Stdout
-	upCmd.Stderr = os.Stderr
+	project, err := loadComposeProject(*template)
+	if err != nil {
+		return err
+	}
 
-	err = upCmd.Run()
+	composeService := compose.NewComposeService(dockerCli)
+	ctx := context.Background()
+
+	err = composeService.Down(ctx, project.Name, api.DownOptions{
+		RemoveOrphans: true,
+		Volumes:       true,
+	})
 	if err != nil {
 		return err
 	}
 
 	return nil
-}
-
-func (d *DockerCompose) resolveComposePath(templateID, path string) (string, error) {
-	if filepath.IsAbs(path) {
-		return path, nil
-	}
-
-	wd, err := os.Getwd()
-	if err != nil {
-		return "", err
-	}
-
-	composePath := filepath.Join(wd, "templates", templateID, path)
-
-	if _, err := os.Stat(composePath); os.IsNotExist(err) {
-		return "", fmt.Errorf("docker-compose file not found: %s", composePath)
-	}
-
-	return composePath, nil
 }
